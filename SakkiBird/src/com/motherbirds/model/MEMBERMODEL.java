b544d7951dffd6f4e564339855343da8654c230b
김영균 <dridy@naver.com>
2017-03-14 18:01:14 +0900

    멤버모델.

---
+++ SakkiBird/src/com/motherbirds/model/MEMBERMODEL.java
@@ -0,0 +1,46 @@
+package com.motherbirds.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class MEMBERMODEL {
+
+	 private String ID;
+	 private String EMAIL;
+	 private String PW;
+	 private String USERNAME;
+	 private Date REGDATE;
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getEMAIL() {
+		return EMAIL;
+	}
+	public void setEMAIL(String eMAIL) {
+		EMAIL = eMAIL;
+	}
+	public String getPW() {
+		return PW;
+	}
+	public void setPW(String pW) {
+		PW = pW;
+	}
+	public String getUSERNAME() {
+		return USERNAME;
+	}
+	public void setUSERNAME(String uSERNAME) {
+		USERNAME = uSERNAME;
+	}
+	public Date getREGDATE() {
+		return REGDATE;
+	}
+	public void setREGDATE(Date rEGDATE) {
+		REGDATE = rEGDATE;
+	}
+	
+	 
+	 
+}
