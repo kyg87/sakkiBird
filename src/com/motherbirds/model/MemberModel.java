5f45d4ce505542d83e4c08fb3a545bb81b4945a6
KimSeungill <1992kimsi@naver.com>
2017-03-22 15:46:22 +0900

    memberinfo, membermodel

---
+++ src/com/motherbirds/model/MemberModel.java
@@ -0,0 +1,47 @@
+package com.motherbirds.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class MemberModel {
+
+	 private String code;
+	 private String email;
+	 private String pw;
+	 private String userName;
+	 private Date regDate;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+	
+	 
+	 
+}
