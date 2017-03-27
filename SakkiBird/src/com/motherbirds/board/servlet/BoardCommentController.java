da634b57b29d1a7e7948b3f128001c02b81d3cf9
동근 <rucro@naver.com>
2017-03-27 17:53:17 +0900

    bc controller

---
+++ SakkiBird/src/com/motherbirds/board/servlet/BoardCommentController.java
@@ -0,0 +1,83 @@
+package com.motherbirds.board.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.motherbirds.dao.BoardCommentDAO;
+import com.motherbirds.dao.MYSQLWriter;
+import com.motherbirds.dao.WriterDao;
+import com.motherbirds.model.WriterModel;
+
+
+@WebServlet("/platz/details")
+public class BoardCommentController extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BoardCommentDAO BCDao = new BoardCommentDAO();
+		String pageNum = request.getParameter("pageNum");
+		WriterDao vote = new MYSQLWriter();
+		WriterModel model = vote.getWriteModel(pageNum);
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String voteId = request.getParameter("vote_id");
+		System.out.print(pageNum);
+		
+		String comment = request.getParameter("message");
+		
+		HttpSession session = request.getSession();
+		
+		System.out.println("username :" +session.getAttribute("member"));
+		
+		if(comment != null){
+			
+			if(session.getAttribute("member") != null){
+				
+				BCDao.addComment(comment,pageNum,(String)request.getSession().getAttribute("member"));
+				response.sendRedirect("details.jsp?page="+pageNum);
+			}
+			
+			else if(session.getAttribute("member") == null)
+			{
+				response.getWriter().println("<script>alert('로그인해라'); location.href='details.jsp?page="+pageNum+"'</script>");
+			}
+			
+		}
+		else{
+			
+			int result = 0;
+			
+			if(request.getSession().getAttribute("member") != null){
+				
+				String userName = (String)request.getSession().getAttribute("member");
+				if(!vote.isVote(pageNum, userName))
+				{
+					vote.voteAdd(model.getCode(), userName);	
+				}
+			}
+		
+			
+			result = vote.update(Integer.parseInt(voteId), pageNum);
+			
+			if(result > 0)
+			{
+				if(request.getSession().getAttribute("member") != null){
+					response.sendRedirect("details?page="+pageNum);	
+				}
+				else{
+					request.setAttribute("result", result);
+					request.getRequestDispatcher("details.jsp?page="+pageNum).forward(request, response);
+				}
+					
+			}
+		}
+	}
+
+}
